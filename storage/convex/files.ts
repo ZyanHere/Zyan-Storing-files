363e5f341db1e408e5cea9f24b114a88261baf6a
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-06-12 23:35:45 +0530

    scope files with orgId

--- storage/convex/files.ts
+++ storage/convex/files.ts
@@ -1,42 +1,37 @@
-import { ConvexError, v } from 'convex/values';
-import { mutation, query } from './_generated/server';
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
-    args: {
-      name: v.string(),
-      fileId: v.id("_storage"),
-      orgId: v.string(),
-    },
-    async handler(ctx, args) {
-      const identity = ctx.auth.getUserIdentity()
+  args: {
+    name: v.string(),
+    orgId: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
 
-      if(!identity){
-        throw new ConvexError("User is not logged in")
-      }
-  
-      await ctx.db.insert("files", {
-        name: args.name,
-        orgId: args.orgId,
-        fileId: args.fileId,
-      });
-    },
-  });
+    if (!identity) {
+      throw new ConvexError("you must be logged in to upload a file");
+    }
 
-  export const getFiles = query({
-    args: {
-      orgId: v.string(),
-      query: v.optional(v.string()),
-      favorites: v.optional(v.boolean()),
-      deletedOnly: v.optional(v.boolean()),
-    },
-    async handler(ctx, args) {
-        const identity = ctx.auth.getUserIdentity()
+    await ctx.db.insert("files", {
+      name: args.name,
+      orgId: args.orgId,
+    });
+  },
+});
 
-        if(!identity){
-          return []
-        }
-  
-      return ctx.db.query('files').collect()
-    },
-  });
+export const getFiles = query({
+  args: {
+    orgId: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
+    return ctx.db.query("files").withIndex('by_orgId', q=>  q.eq("orgId", args.orgId)).collect()
+  },
+});
   
